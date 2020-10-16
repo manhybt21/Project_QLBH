c6667d5682cae3d2dc0b40d3b377a3e11aa73830
Doãn Hải <honghai3501@gmail.com>
2020-10-16 22:13:40 +0700

    fix

--- manh/DAL/DBConnected.cs
+++ manh/DAL/DBConnected.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DAL
 {
     public static class DBConnected
     {
-        internal static SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DuAnMau;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        internal static SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["QLBanHang"].ToString());
     }
 }
